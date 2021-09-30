a67fa358403673b1423efa4c9ffb5b6f306b73f3
fastcode-inc <60857811+fastcode-inc@users.noreply.github.com>
2021-09-30 13:39:47 -0400

    untitled-app updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { ErrorRoutingModule } from './error-routing/error-routing.module';
 import { View1Component } from './view1/view1.component';
 import { View2Component } from './view2/view2.component';
 import { View3Component } from './view3/view3.component';
-import { ChildViewComponent } from './child-view/child-view.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'view1', pathMatch: 'full' },
@@ -15,7 +14,6 @@ export const routes: Routes = [
   { path: 'view1', component: View1Component, data: { text: 'View 1' } },
   { path: 'view2', component: View2Component, data: { text: 'View 2' } },
   { path: 'view3', component: View3Component, data: { text: 'View 3' } },
-  { path: 'child-view', component: ChildViewComponent, data: { text: 'Child View' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
 
